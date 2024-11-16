bd185dd85bffda9cf65f1fbdd0e7937dfad20cde
KamilloDev <krystiankowalczyk1204@gmail.com>
2024-11-16 15:14:57 +0100

    Render

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ require('dotenv').config(); // Load environment variables
 const { Client } = require('pg');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(bodyParser.json());
