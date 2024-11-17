da823e13be5ef5a5d7df3e0135099d64431b7f86
KamilloDev <krystiankowalczyk1204@gmail.com>
2024-11-17 15:03:40 +0100

    Added data for map

--- kort/data.js
+++ kort/data.js
@@ -29,5 +29,47 @@ const countriesData = [
         hvornårEutanasi: '2001',
         lovligtUdland: 'Ikke tilladt'
     },
+    {
+        id: 'PT',
+        name: 'Portugal',
+        eutanasiType: 'aktiv dødshjælp',
+        hvornårEutanasi: '2023',
+        lovligtUdland: 'Ikke tilladt'
+    },
+    {
+        id: 'DE',
+        name: 'Tyskland',
+        eutanasiType: 'assisteret dødshjælp',
+        hvornårEutanasi: '2020',
+        lovligtUdland: 'Ikke tilladt'
+    },
+    {
+        id: 'BE',
+        name: 'Belgien',
+        eutanasiType: 'aktiv dødshjælp',
+        hvornårEutanasi: '2002',
+        lovligtUdland: 'Tilladt'
+    },
+    {
+        id: 'LU',
+        name: 'Luxemborg',
+        eutanasiType: 'aktiv dødshjælp',
+        hvornårEutanasi: '2009',
+        lovligtUdland: 'Tilladt'
+    },
+    {
+        id: 'IT',
+        name: 'Italien',
+        eutanasiType: 'assisteret dødshjælp',
+        hvornårEutanasi: '2019',
+        lovligtUdland: 'Ikke tilladt'
+    },
+    {
+        id: 'AT',
+        name: 'Østrig',
+        eutanasiType: 'assisteret dødshjælp',
+        hvornårEutanasi: '2022',
+        lovligtUdland: 'Ikke tilladt'
+    },
     // Tilføj flere lande efter behov
 ];
